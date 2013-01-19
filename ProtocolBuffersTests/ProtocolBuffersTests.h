9ec08aaad963318c27770a941cc17948c5ec1228
dzhurov <dzhurov@provectus-it>
2013-01-19 12:56:47 +0200

    Init

---
+++ ProtocolBuffersTests/ProtocolBuffersTests.h
@@ -0,0 +1,13 @@
+//
+//  ProtocolBuffersTests.h
+//  ProtocolBuffersTests
+//
+//  Created by Dmitry Zhurov on 17.10.12.
+//  Copyright (c) 2012 Dmitry Zhurov. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ProtocolBuffersTests : SenTestCase
+
+@end
---
+++ ProtocolBuffersTests/ProtocolBuffersTests.m
@@ -0,0 +1,32 @@
+//
+//  ProtocolBuffersTests.m
+//  ProtocolBuffersTests
+//
+//  Created by Dmitry Zhurov on 17.10.12.
+//  Copyright (c) 2012 Dmitry Zhurov. All rights reserved.
+//
+
+#import "ProtocolBuffersTests.h"
+
+@implementation ProtocolBuffersTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ProtocolBuffersTests");
+}
+
+@end
